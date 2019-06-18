185b90f500a15c06bb0a92122ac7490b8feb46c0
Jeff A <almanzarortizjeffrey@gmail.com>
2019-06-18 13:26:40 -0400

    folders modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
 import Home from './components/Home';
-=======
-import Home from './Component/Home';
-import LogIn from './Component/LogIn';
-import Register from './Component/Register';
->>>>>>> 5a2d3510cbd492919d70bce4b23fd0971c8b9c2f
+import LogIn from './components/LogIn';
+import Register from './components/Register';
 
 function App() {
   return (
