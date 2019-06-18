64ae1b4c14d25a327498ea8da76e10484a8fa0df
Jeff A <almanzarortizjeffrey@gmail.com>
2019-06-18 14:11:23 -0400

    products page sample added

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Register from './components/Register'
+import Register from './components/Register';
+import Products from './components/Products';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
