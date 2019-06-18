83ce0ae02c5769f19fbcad7888c68ce651bb3885
Jeff A <almanzarortizjeffrey@gmail.com>
2019-06-18 13:12:25 -0400

    modified folders

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../CSS/login.css';
+import '../css/login.css';
 
 class Register extends React.Component{
     constructor(props){
--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Register from './Component/Register'
+import Register from './components/Register'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Register />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
